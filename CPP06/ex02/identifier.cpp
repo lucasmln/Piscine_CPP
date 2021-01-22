9fb0d08ed034aa8d3938e93861af5b61b15e5604
lmoulin <lucas.moulin78114@gmail.com>
2021-01-22 11:34:27 +0100

    clean

--- CPP06/ex02/identifier.cpp
+++ CPP06/ex02/identifier.cpp
@@ -2,19 +2,19 @@
 
 void identify_from_pointer(Base * p)
 {
-    A *a = dynamic_cast<A*>(p);
-    B *b = dynamic_cast<B*>(p);
-    C *c = dynamic_cast<C*>(p);
+	A *a = dynamic_cast<A*>(p);
+	B *b = dynamic_cast<B*>(p);
+	C *c = dynamic_cast<C*>(p);
 
-    if(a != NULL)
-        std::cout << "A" << std::endl;
-    if (b != NULL)
-        std::cout << "B" << std::endl;
-    if (c != NULL)
-        std::cout << "C" << std::endl;
+	if (a != NULL)
+		std::cout << "A" << std::endl;
+	if (b != NULL)
+		std::cout << "B" << std::endl;
+	if (c != NULL)
+		std::cout << "C" << std::endl;
 }
 
 void identify_from_reference(Base & p)
 {
-    identify_from_pointer(&p);
+	identify_from_pointer(&p);
 }
